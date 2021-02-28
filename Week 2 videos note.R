7bcba4813e81b9290440d2ee3979a5de2b9ad23c
Craken651 <craken651@gmail.com>
2021-02-28 12:46:26 +0000

    note on 1st 6 videos

---
+++ Week 2 videos note.R
@@ -0,0 +1,129 @@
+## VIDEO 1
+# 2nd way of assinging value of y
+if(x>3) {
+  y<-10
+} else {
+  y<-0
+}
+print(y)
+
+# 2nd way of assinging value of y
+y<- if(x>3) {
+  10
+} else {
+  0
+}
+
+print(y)
+
+## VIDEO 2
+#for loop
+for(i in 1:10){
+  print(i)
+}
+
+x <- c("a", "b", "c", "d")
+for(i in 1:4){
+  print(x[i])
+}
+
+for(i in seq_along(x)){
+  print(x[i])
+}
+
+for(letter in x){
+  print(letter)
+}
+
+for(i in 1:4) print(x[i])
+
+
+# nested for loops (nesting beyond 2-3 levels can be very difficult to read/understand)
+x<- matrix(1:6, 2, 3)
+
+for(i in seq_len(nrow(x))){
+  for (j in seq_len(ncol(x))) {
+    print(x[i, j])
+    }
+}
+
+
+## VIDEO 3
+# while loop
+# They begin by testing a condition. If true, then hey execute the loop body.
+# Once the loop body is executed, the condition is tested again, and so forth 
+# **!!while loops can potentially result in infinite loops if not written properly!!**
+ count <- 0
+ while(count < 10) {
+   print(count)
+   count<- count +1
+ }
+ print(count)
+ 
+ # there can be more than one condition in the test 
+ z <- 5 
+ 
+ while( z >= 3 && z <= 10) {
+   print(z)
+   coin <- rbinom(1, 1, 0.5)
+   
+   if(coin == 1) { ## random walk
+     z <- z + 1
+   } else {
+       z <- z-1 
+     }
+ }
+
+ 
+ ## VIDEO 4
+ # Reoeat loop 
+ # repeat inititates an infinite loop: These are not commonly used in 
+ # statistical applications but the do have their uses. 
+ # The only way to exit a repeat loop is to call break 
+
+ x0 <- 1
+ tol <- le-8
+ # le-8 should = 10^-8
+ 
+ repeat {
+   xl <- computeEstimate()
+   # computeEstimate() is not a real function
+   
+   if(abs(x1-x0) < tol){
+     break
+   } else {
+     x0 <-x1
+   }
+ }
+  
+ # there is no guarantee that this loop will stop and report whether convergence was achieved.
+ 
+ #Next and return  loop 
+ for(i in 1:100) {
+   if(i <= 20) {
+     #skipp the first 20 iteration
+     next()
+   }
+   ## do something here
+   print(i)
+ }
+ # retrun signals that a function should exit and return a given value
+ 
+ 
+ 
+ ## VIDEO 5
+ # Repeat, Break, Next
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
